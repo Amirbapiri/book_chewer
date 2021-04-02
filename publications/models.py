6e7f883af81083158184a83af706da023fe8cf60
amir <amirbapiri@gmail.com>
2021-04-02 12:00:26 +0430

    feat: create Publication model
    This commit creates Publication model in order to store information of a
    specific publication

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class Publication(models.Model):
+    user = models.OneToOneField(get_user_model(), null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=125, blank=False, null=False)
+    reg_number = models.CharField(max_length=12, blank=False, null=False)
+    date_of_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Publication {str(self.name)}"
