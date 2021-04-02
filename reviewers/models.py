a8b898ea5282f5898812da2354796f6485c8a3d3
amir <amirbapiri@gmail.com>
2021-04-02 22:15:29 +0430

    feat: create model reviewer
    Each reviewer is a user. However, we defined custom model to save it's
    information.

--- reviewers/models.py
+++ reviewers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Reviewer(models.Model):
+    class ReviewerExperience(models.IntegerChoices):
+        BEGINNER = 0, 'Beginner'
+        INTERMEDIATE = 1, 'Intermediate'
+        EXPERT = 2, 'Expert'
+
+    user = models.OneToOneField(get_user_model(), null=True, on_delete=models.SET_NULL)
+    experience = models.SmallIntegerField(
+        choices=ReviewerExperience.choices,
+        default=ReviewerExperience.BEGINNER
+    )
+
+    def __str__(self):
+        return self.user.email
