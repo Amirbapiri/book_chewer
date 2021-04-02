fa04ce8909b8088c9b79f9673d54a674a861152c
amir <amirbapiri@gmail.com>
2021-04-02 12:01:48 +0430

    feat: create panel admin for publication

--- publications/admin.py
+++ publications/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Publication
+
+
+@admin.register(Publication)
+class PublicationModelAdmin(admin.ModelAdmin):
+    fieldsets = (
+        # Personal info of user
+        ("Publication Information", {"fields": (
+            "user",
+            "name",
+            "reg_number",
+        )}),
+    )
+    list_display = (
+        "user",
+        "name",
+        "reg_number",
+        "date_of_create",
+    )
+
+    search_fields = (
+        "name",
+        "reg_number"
+    )
